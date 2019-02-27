9c6fb11cba87bfb01aff86f7c23469f4fdfc7e47
ThePackPater <thepackpater@gmail.com>
2019-02-27 14:01:39 -0600

    slooooow and steady

--- myjs.js
+++ myjs.js
@@ -1,11 +1,10 @@
 
-var shows = ["firefly", "Star Trek", "Stargate SG1", "Battlestar Galactica"];
+var shows = ["Firefly", "Star Trek", "Stargate SG1", "Battlestar Galactica"];
 
 function displayShowGIF() {
 
     var gif = $(this).attr("data-name");
-
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+    var queryURL = "https://api.giphy.com/v1/gifs/?t=" + shows + "api_key=UgvnXDeFmvBkNefMzHfK5ZhsJlT0L0ex&q=scifi%20shows&limit=10&&lang=en";
 
     $.ajax({
         url: queryURL,
@@ -17,13 +16,17 @@ function displayShowGIF() {
         var gifDiv = $("<div class='gifs'>");
         var rated = response.rating;
         var pRate = $("<p>").text("Rating: " + rated);
-        var gif = response.fixed_height_still;
+        var gifImage = response.fixed_height_still;
         var gifStill = $("<img>").attr("src", gif);
 
         gifDiv.append(pRate);
-        gifDiv.append(gif);
+
+        gifDiv.append(gifImage);
+
         gifDiv.append(gifStill);
+
         $("#gifsDiv").prepend(gifDiv);
+
     });
 }
 
@@ -31,21 +34,40 @@ function buttonMaker() {
 
     $("#buttons-view").empty();
 
-    for (var i = 0; i < gifs.length; i++) {
+    for (var i = 0; i < shows.length; i++) {
 
         var x = $("<button>");
 
         x.addclass("giphy btn btn-primary");
 
-        x.attr("data-name", gifs[i]);
+        x.attr("data-name", shows[i]);
 
-        x.text(gifs[i]);
+        x.text(shows[i]);
 
-        ("#buttons-view").append(x);
+        $("#buttons-view").append(x);
 
     }
 
 }
+
+
+  $("#addShow").on("click", function(event) {
+    
+    event.preventDefault();
+    
+    var gif = $("#show-input").val().trim();
+
+     shows.push(gif);
+
+    $.unique(shows);
+   
+    buttonMaker();
+
+  });
+
+  $(document).on("click", "giphy btn btn-primary", displayShowGIF);
+
+  buttonMaker();
 
 
 
